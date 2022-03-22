d2077f2073214a125f94fba94b4878f5d291dcb5
Cindy <cindy30446@gmail.com>
2022-03-22 13:14:16 +0800

    pw

---
+++ pw.py
@@ -0,0 +1,12 @@
+password = '01071126'
+x = 3
+while True:
+	pwd = input('請輸入密碼:')
+	if pwd == password:
+		print('登入成功!')
+		break
+	else:
+		x = x - 1
+		print('密碼輸入錯誤!還有', x,'次機會!')
+		if x == 0:
+			break
